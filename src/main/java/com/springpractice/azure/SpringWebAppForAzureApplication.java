feba8e8e58154ef3fd47b6a7573fbe58578204bb
Abhinav Pandey <apandey48@sapient.com>
2021-05-16 17:25:26 +0530

    Adding docker file

---
+++ src/main/java/com/springpractice/azure/SpringWebAppForAzureApplication.java
@@ -0,0 +1,13 @@
+package com.springpractice.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppForAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppForAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springpractice/azure/SpringWebAppForAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppForAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
